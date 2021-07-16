40c76191b89308ddf509752fe534ac8a31cc598b
Muhammad Dawood <md802628@gmail.com>
2021-07-16 15:40:14 +0500

    Initial commit

---
+++ src/main/java/mynearmarketdatastore/mynearmarketdatastore/MynearmarketDatastoreApplication.java
@@ -0,0 +1,13 @@
+package mynearmarketdatastore.mynearmarketdatastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynearmarketDatastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynearmarketDatastoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mynearmarketdatastore/mynearmarketdatastore/MynearmarketDatastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mynearmarketdatastore.mynearmarketdatastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynearmarketDatastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
